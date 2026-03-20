166a79b9e37ca76fd2830c00f06dc7e96bf430c8
mpatino <mspatino@gmail.com>
2026-03-20 13:38:15 -0300

    agrego Cie10DTO y mejoras

---
+++ src/main/java/com/consultorio/pacientes/dtos/Cie10DTO.java
@@ -0,0 +1,14 @@
+package com.consultorio.pacientes.dtos;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class Cie10DTO {
+
+    private String codigo;
+    private String descripcion;
+}
